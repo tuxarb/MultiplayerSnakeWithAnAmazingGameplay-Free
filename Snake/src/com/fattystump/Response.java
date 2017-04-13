38569c6983c31a00fe3864fca2c8b8107767a4f0
ph4rr3ll <pharrell@ya.ru>
2017-04-13 21:32:53 +0300

    Добавлены классы Request и Response для взаимодействия клиента с сервером

---
+++ Snake/src/com/fattystump/Response.java
@@ -0,0 +1,17 @@
+package com.fattystump;
+
+/**
+ * Created by vkhly on 13.04.2017.
+ */
+public class Response {
+
+    public String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
