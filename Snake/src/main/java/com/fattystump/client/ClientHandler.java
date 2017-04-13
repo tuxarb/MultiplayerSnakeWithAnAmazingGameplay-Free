353d67a117706f4198752b00818cd1418379a853
Tuxarb <tuxarb777@yandex.ru>
2017-04-13 23:55:13 +0300

    added maven and kryonet lib.

--- Snake/src/main/java/com/fattystump/client/ClientHandler.java
+++ Snake/src/main/java/com/fattystump/client/ClientHandler.java
@@ -7,21 +7,17 @@ import java.awt.event.KeyListener;
 class ClientHandler {
 
     void startHandling() {
-
     }
 
     class KeywordHandler implements KeyListener {
-        @Override
         public void keyTyped(KeyEvent e) {
 
         }
 
-        @Override
         public void keyPressed(KeyEvent e) {
 
         }
 
-        @Override
         public void keyReleased(KeyEvent e) {
 
         }
