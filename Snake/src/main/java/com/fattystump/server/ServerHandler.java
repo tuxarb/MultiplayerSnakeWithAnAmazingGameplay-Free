007ed9ffe133a31c57af7e61ccba70bea32571e3
ph4rr3ll <pharrell@ya.ru>
2017-04-14 23:09:26 +0300

    Добавил пустышку класса ServerHandler - обработчик серверных запросов.

---
+++ Snake/src/main/java/com/fattystump/server/ServerHandler.java
@@ -0,0 +1,16 @@
+package com.fattystump.server;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ServerHandler implements ActionListener {
+
+    public ServerHandler(ServerFrame serverFrame) {
+        
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
